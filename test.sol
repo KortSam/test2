c818420f6c379d6c21cf7ac2f18c3961b7c68b76
KortSam <samkort@hotmail.nl>
2023-06-06 22:57:11 +0200

    Add files via upload

---
+++ test.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.8.1;
+pragma experimental ABIEncoderV2;
+contract CarRegistry {
+
+struct Car {
+        string licensePlate;
+        string chassisNumber;
+        string brand;
+        string carType;
+        string color;
+        string[] pictures;
+        uint mileage;
+        uint askingPrice;
+        address currentOwner;
+        bool isForSale;
+    }
+
+    mapping(string => Car) cars;
+    string[] all_licenceplates;
+}
+
+
